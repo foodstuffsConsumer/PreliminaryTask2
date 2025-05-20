9182ccae2bafe4a7f32e9f772ba3c7df8c5ce76b
foodstuffsConsumer <160200985+foodstuffsConsumer@users.noreply.github.com>
2025-05-20 15:09:33 +1000

    c

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
- 
+import time
+
+def main():
+
+    while True:
+
+        print('\nChoose an action!')
+        print('\n1 - Attack')
+        print('2 - Skill')
+        print('3 - Other Skill')
+        print('4 - Use Item')
+        command = input('\nType in the corresponding number! ')
+
+        if command == '1':
+            print("You tried to attack... and MISSED! you tried")
+        elif command == '2':
+            print("wait hold on i haven't even coded this in yet")
+        elif command == '3':
+            print("You didn't use Other Skill!")
+        elif command == '4':
+            print("ITEMS: Fish, fish-shaped medkit, fish-shaped sediment, fish-shaped toilet, fish-shaped fish. You don't like fish so all of these are unusuable, sorry.")
+
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
